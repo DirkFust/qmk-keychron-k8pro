e90e88483087fdefe087daefcd58cde42cba6406
Dirk Fust <dirk.fust@outlook.de>
2023-10-18 18:50:24 +0200

    initial commit

---
+++ test/config.h
@@ -0,0 +1,8 @@
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+// Mousekeys https://docs.qmk.fm/#/feature_mouse_keys
+#define MK_COMBINED  //Combined mode
+#define MOUSEKEY_MAX_SPEED 5
+
+// Caps Word https://docs.qmk.fm/#/feature_caps_word
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
